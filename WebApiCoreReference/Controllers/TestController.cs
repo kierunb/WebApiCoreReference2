a26c8b9192ef8201a451b68987512a88e72b76b6
kierunb <kierunb@gmail.com>
2020-10-16 11:35:39 +0200

    Add project files.

---
+++ WebApiCoreReference/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiCoreReference.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }   
+        
+        [HttpGet("[action]")]
+        public IActionResult Ping()
+        {
+            var response = new { Pong = "Pong" };
+            return Ok(response);
+        }
+    }
+}
